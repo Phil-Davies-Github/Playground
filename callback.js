f6976b3ae47865591593066e15e4043bb2f16481
Anthony Farncombe <anthony.farncombe@redpack.co.uk>
2020-06-15 15:05:26 +0100

    Bug fixes

--- callback.js
+++ callback.js
@@ -1,20 +1,20 @@
 const posts = [
     {title: 'Post One', body: 'This is post one'},
     {title: 'Post Two', body: 'This is post two'}
-]
+];
 
 function getPosts(){
     setTimeout(function() {
         var output = '';
-        posts.forEach((post, index) => {
-            output += '<li>${post.title}</li>';
+        posts.forEach(post => {
+            output += `<li>${post.title}</li>`;
         });
         document.body.innerHTML = output;
-    }, 1000)
+    }, 1000);
 }
 
 function createPost(post) {
-    setTimeout(function(){
+    setTimeout(function() {
         posts.push(post);
     }, 2000);
 }
@@ -23,5 +23,5 @@ getPosts();
 
 createPost({title: 'Post Three', body: 'This is post two'});
 
-console.log('title : ${post.title}');
-console.log('title : ${post.title}');
+console.log(`title : ${posts[0].title}`);
+console.log(`title : ${posts[1].title}`);
