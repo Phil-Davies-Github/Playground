cf73708e895e94e29129a350e10d1f919703c58e
Phil Davies <phil.davies@redpack.co.uk>
2020-06-16 14:08:23 +0100

    Includes Async await and promises

--- callback.js
+++ callback.js
@@ -1,6 +1,7 @@
 const posts = [
     {title: 'Post One', body: 'This is post one'},
-    {title: 'Post Two', body: 'This is post two'}
+    {title: 'Post Two', body: 'This is post two'},
+    {title: 'Post Three', body: 'This is post three'}
 ];
 
 function getPosts(){
@@ -13,15 +14,16 @@ function getPosts(){
     }, 1000);
 }
 
-function createPost(post) {
+function createPost(post, callback) {
     setTimeout(function() {
         posts.push(post);
+        callback();
     }, 2000);
 }
 
 getPosts();
 
-createPost({title: 'Post Three', body: 'This is post two'});
+createPost({title: 'Post Four', body: 'This is post four'}, getPosts);
 
 console.log(`title : ${posts[0].title}`);
 console.log(`title : ${posts[1].title}`);
